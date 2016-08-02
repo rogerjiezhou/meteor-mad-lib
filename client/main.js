ffccc3178ed7b45f0412afbbc4968c5c494eeee7
avisochek <avisochek3@gmail.com>
2016-08-01 20:27:29 -0400

    added console loggin for madlib thing

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
 Template.body.events({
-  'submit #form'(event) {
+  'submit #form'(event,template) {
     event.preventDefault();
-    console.log("hello, world");
+    let exclamation = event.target.exclamation.value;
+    let adverb = event.target.adverb.value;
+    let noun = event.target.noun.value;
+    let verb = event.target.verb.value;
+    console.log(exclamation+"! you say, as you "+adverb+" jump onto your "+noun+" and "+verb+" into the sunset.");
   }
 })
