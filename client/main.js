33ecd30ad26ea976dcf1d48221a73eea868cbc9c
Jie Zhou <zhoujie@my.bridgeport.edu>
2016-08-01 20:41:58 -0400

    add fade in effect

--- client/main.js
+++ client/main.js
@@ -5,6 +5,10 @@ Template.body.events({
     let adverb = event.target.adverb.value;
     let noun = event.target.noun.value;
     let verb = event.target.verb.value;
-    console.log(exclamation+"! you say, as you "+adverb+" jump onto your "+noun+" and "+verb+" into the sunset.");
+    let madLib = exclamation+"! you say, as you "+adverb+" jump onto your "+noun+" and "+verb+" into the sunset.";
+    console.log(madlib);
   }
+
+  $('#madLib').fadeIn('slow',,);
+
 })
