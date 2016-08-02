ff04477f8ec6e01845134dcb6a9e781b1afbd862
avisochek <avisochek3@gmail.com>
2016-08-01 20:48:10 -0400

    fixes

--- client/main.js
+++ client/main.js
@@ -6,9 +6,8 @@ Template.body.events({
     let noun = event.target.noun.value;
     let verb = event.target.verb.value;
     let madLib = exclamation+"! you say, as you "+adverb+" jump onto your "+noun+" and "+verb+" into the sunset.";
-    console.log(madlib);
+    // console.log(madlib);
+    $('#madLib').hide().text(madLib).fadeIn('slow');
   }
-
-  $('#madLib').fadeIn('slow',,);
 
 })
